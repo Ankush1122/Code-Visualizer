a6e09cf02ae677ffd9a80b4317d113bad7570ad2
Ankush <lothermeyer@protonmail.com>
2023-03-15 20:02:39 +0530

    added user input

--- base/views.py
+++ base/views.py
@@ -3,13 +3,26 @@ import sys
 from django.shortcuts import render
 
 def home(request):
-    context = {'error':False, 'output':""} 
+    context = {'error':False, 'output':"", 'code':"", 'input':""} 
     return render(request, 'base/index.html', context)
 
 
 def execute_code(request):
     if(request.method == "POST"):
         code = request.POST['code']
+        user_input = request.POST['input']
+
+        d = user_input.split("\n")
+
+        def input():
+            try:
+                out = d[0]
+                d.pop(0)
+            except:
+                raise Exception("Enter Inputs")
+
+            return out
+
         try:
             original_stdout = sys.stdout
             sys.stdout = open('file.txt', 'w')
@@ -17,11 +30,11 @@ def execute_code(request):
             sys.stdout.close()
             sys.stdout = original_stdout
             output = open('file.txt', 'r').read()
-            context = {'error':False, 'output':output}
+            context = {'error':False, 'output':output, 'code':code, 'input':user_input}
 
         except Exception as e:
             sys.stdout = original_stdout
-            context = {'error':True, 'output':e}
+            context = {'error':True, 'output':e, 'code':code, 'input':user_input}
     print(context)
     return render(request, 'base/index.html', context)
 
