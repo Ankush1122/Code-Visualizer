e3b6a1f23515a069288c0ab97a71401c88e5e624
Ankush <dankushdeshmukh@gmail.com>
2024-10-10 02:03:46 +0530

    frontend bugs fixed

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-d = ['']
+d = ['13']
 def input():
     try:
         out = d[0]
@@ -8,5 +8,21 @@ def input():
 
     return out 
 
-for i in range(3):
-    print(i)
+# Check if number is Prime or Not
+# Take input from the user
+n = int(input())
+
+# Check if the number is prime
+prime = True
+if n <= 1:
+    prime = False
+else:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            prime = False
+            break
+
+if (prime):
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is not a prime number.")
